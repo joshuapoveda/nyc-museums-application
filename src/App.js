28f1f686f3b7081165fd34f9f3d540ba9e3d64a1
Joshua Poveda <joshuapoveda9@gmail.com>
2023-04-25 11:45:59 -0400

    TEST

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import logo from './logo.svg';
+import { useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  //define state
+  const [city, setCity] = useState(null)
+
+  //const rootUrl = 'https://api.teleport.org/api/cities/?search=san%20francisco'
+
+  const getCity = async () => {
+    //make fetch request and store response
+    try{
+    const response = await fetch(
+      `https://data.cityofnewyork.us/resource/fn6f-htvy.json?name=Alexander Hamilton U.S. Custom House`
+    );
+    //parse JSON response into a js object
+    const data = await response.json();
+    setCity(data);
+    } catch(e){
+      console.error(e)
+    }
+  };
+  
+  useEffect(()=>{getCity()},[])
+  
+  console.log(city)
+  console.log(typeof city)
+  
+  
+
   return (
     <div className="App">
-  
+      <h1>Hello World</h1>
+      <ul>
+
+      </ul>
     </div>
   );
 }
