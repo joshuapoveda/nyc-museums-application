4919059a64f9b3d7fb1553dcbb850ed3da9f6433
Joshua Poveda <joshuapoveda9@gmail.com>
2023-05-02 12:09:53 -0400

    rendering

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,20 +2,24 @@ import { useState, useEffect } from "react";
 import MapImg from "./MapImg";
 
 export default function Display(props) {
-  const [coordsProp, setCoordsProp] = useState([-73.99392890421056, 40.738015384101416]);
+  const [coordsProp, setCoordsProp] = useState([
+    -73.99392890421056, 40.738015384101416,
+  ]);
   const [cardsProp, setCardsProp] = useState([]);
 
   const data = props.itemsArr;
 
-  const coordsFunc = (e) =>{
-    console.log(e.currentTarget.children[4].innerText)
-    setCoordsProp([parseFloat(e.currentTarget.children[4].innerText),parseFloat(e.currentTarget.children[5].innerText)])
-  }
+  const coordsFunc = (e) => {
+    console.log(e.currentTarget.children[4].innerText);
+    setCoordsProp([
+      parseFloat(e.currentTarget.children[4].innerText),
+      parseFloat(e.currentTarget.children[5].innerText),
+    ]);
+  };
 
   const cards = data.map((museums, index) => {
     return (
       <div className="cards" key={index} onClick={coordsFunc}>
-        
         <a href={museums.url} target="_blank">
           <p>{museums.name}</p>
         </a>
@@ -23,16 +27,18 @@ export default function Display(props) {
         <p>{museums.city}</p>
         <p>{museums.tel}</p>
 
-        <button name="map-btn">{museums.coordsLong} {museums.coordsLat}</button>
+        <button name="map-btn">
+          {museums.coordsLong}<br></br>{museums.coordsLat}
+        </button>
         <p className="none">{museums.coordsLat}</p>
         <p className="none">{museums.coordsLong}</p>
       </div>
     );
   });
 
-  useEffect(()=>{
-    setCardsProp(cards)
-  },[])
+  useEffect(() => {
+    setCardsProp(cards);
+  }, []);
 
   return (
     <div>
