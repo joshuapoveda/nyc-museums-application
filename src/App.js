cd9e241d67196b7ff1353b58dbdf10edfa687b29
Joshua Poveda <joshuapoveda9@gmail.com>
2023-05-02 15:25:36 -0400

    final build

--- src/App.js
+++ src/App.js
@@ -2,22 +2,6 @@ import "./App.css";
 import Museum_Hat from "./img/Museum_Hat.jpg";
 import Form from "./components/Form";
 
-import {
-  interaction,
-  layer,
-  custom,
-  control,
-  Interactions,
-  Overlays,
-  Controls,
-  Map,
-  Layers,
-  Overlay,
-  Util,
-  Tile,
-  Vector,
-} from "react-openlayers";
-
 import { useState, useEffect } from "react";
 
 export default function App() {
@@ -50,33 +34,7 @@ export default function App() {
   //const place = [-73.92079629906307, 40.644340681393366];
   return (
     <div className="App">
-      <div className="map-container">
-        <Map
-          className="map-item"
-          view={{
-            projection: "EPSG:4326",
-            center: [-73.92079629906307, 40.644340681393366],
-            zoom: 17.5,
-          }}
-          style={{ height: "400px", width: "400px" }}
-        >
-          <Controls>
-            <control.FullScreen />
-            <control.ZoomSlider />
-            <control.ZoomToExtent />
-            <control.Zoom />
-          </Controls>
-          <Interactions
-            mouseWheelZoom={false}
-            dragAndDrop={false}
-            dragPan={false}
-            KeyboardPan={true}
-          ></Interactions>
-          <Layers>
-            <layer.Tile></layer.Tile>
-          </Layers>
-        </Map>
-      </div>
+      
       <div className="parallax">
         <img src={Museum_Hat} alt="Museum Hat Mascot" />
       </div>
